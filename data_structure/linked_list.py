84707a40c71fa10740a61722cd4c5d0e51a42a25
chenjianhong <1815515831@qq.com>
2015-03-13 00:54:54 +0800

    linkedlist continue...

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -18,17 +18,38 @@ class LinkedList(object):
         self.size = 0
 
     def add(self,value):
-        node = LinkedList.Node(value,None,None)
+        node = self.get_node(self.size)
+        self.add_before(self.size,value)
+
+    def add_before(self,position_node,value):
+        node = LinkedList.Node(value,position_node.prev,position_node)
+        position_node.prev.next = node
+        position_node.next = node
 
     def get_node(self,index):
         if index < 0 or index > self.size:
             raise Exception('size error')
         if index < self.size/2:
             tmp = self.begin_marker
-            for i in range(0,self.size):
+            for i in range(0,index):
                 tmp = tmp.next
         else:
             tmp = self.end_maker
-            for i in range(self.size,0,-1):
-                tmp = tmp.next
+            for i in range(self.size,index,-1):
+                tmp = tmp.prev
         return tmp
+
+    def __iter__(self):
+        return LinkedListIter(self.begin_marker,self.end_maker)
+
+
+class LinkedListIter(object):
+
+    def __init__(self,begin_marker,end_marker):
+        self.begin_marker = begin_marker
+        self.end_marker = end_marker
+        self.current = begin_marker.next
+        self.count = 0
+
+    def next(self):
+        pass
